03abc8b02a437e32cd9739dbfba30c4845e0767d
Hemant <hemantdhamal7@gmail.com>
2024-08-15 14:05:49 +0530

    Created positive test case for product count

--- Acadamy-Training/JDBC/jdbc/src/test/java/com/spring/jdbc/JdbcApplicationTests.java
+++ Acadamy-Training/JDBC/jdbc/src/test/java/com/spring/jdbc/JdbcApplicationTests.java
@@ -1,13 +1,29 @@
 package com.spring.jdbc;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 @SpringBootTest
 class JdbcApplicationTests {
 
+	public static final String query = "select count(*) from product";
+
 	@Test
 	void contextLoads() {
+	}
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
+
+	@Test
+	private void testProductCount() {
+		long productCount = jdbcTemplate.queryForObject(query, Long.class);
+		assertThat(productCount).isEqualTo(3);
+
 	}
 
 }
