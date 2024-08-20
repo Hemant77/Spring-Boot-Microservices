980a46885f1d99cb125a6cc8ae51bf746698531e
Hemant <hemantdhamal7@gmail.com>
2024-08-20 22:36:35 +0530

    Spring Boot RESTful application-Initial commit

---
+++ Acadamy-Training/REST/rest/src/main/java/com/spring/rest/controller/ProductController.java
@@ -0,0 +1,12 @@
+package com.spring.rest.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+	private final Logger logger = LoggerFactory.getLogger(ProductController.class);
+
+	
+}
